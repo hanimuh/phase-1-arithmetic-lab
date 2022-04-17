b71c8739d23d9539020dfc7864ac6f360399df4f
Hani <md.hani@hotmail.com>
2022-04-18 02:38:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2;
+let num2=  31;
+let multiply = num1 *num2;
+let x= Math.random()*100;
+let random = Math.floor(x);
+let num3= 19;
+let num4= 5;
+let mod= num3%num4;
+let max= Math.max (4, 7, 12, 20)
